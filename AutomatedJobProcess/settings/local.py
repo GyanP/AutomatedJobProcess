ca7580f9668be09b5e0e2d487fd5ad8ce9b34fd3
GyanP <gyanig72@gmail.com>
2019-03-13 17:07:47 +0530

    Add automated-job-process project root folder

---
+++ AutomatedJobProcess/settings/local.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS += []
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', ''),
+        'PORT': '5432',
+    }
+}
